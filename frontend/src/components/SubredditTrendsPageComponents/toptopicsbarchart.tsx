7c15456d3d3addfc58e0ea105f08c73b38c1db8c
chenvincen <71801988+chenvincen@users.noreply.github.com>
2025-02-20 03:18:12 -0800

    removed unused dep

--- frontend/src/components/SubredditTrendsPageComponents/toptopicsbarchart.tsx
+++ frontend/src/components/SubredditTrendsPageComponents/toptopicsbarchart.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { TrendingUp } from "lucide-react"
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+import { Bar, BarChart, XAxis } from "recharts"
 import {
   Card,
   CardContent,

f6addf2833eeb7a265d7b21d62390be2e99aeacf
Vchen7629 <chenvincent7629@gmail.com>
2025-03-10 21:04:24 -0700

    added basic functionality for creating new users in postgres table

--- loginapi/internal/db_connection/dbconn.go
+++ loginapi/internal/db_connection/dbconn.go
@@ -6,34 +6,62 @@ import (
 	"log"
 	"time"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/vchen7629/cyphria/login-api/config/db_config"
+	"github.com/vchen7629/cyphria/login-api/config/poolconfig"
 )
 
-func ConnectDatabase() {
-	dbconn, err := pgxpool.NewWithConfig(context.Background(), dbconfig.Config())
+var DBConn *pgxpool.Pool
+
+func ConnectDatabase() error {
+	dbconn, err := pgxpool.NewWithConfig(context.Background(), config.PoolConfig())
 	if err != nil {
-		log.Fatal("Error Connecting to the database")
+		return fmt.Errorf("error connecting to the database: %w", err)
 	}
 
-	defer dbconn.Close()
 	err = dbconn.Ping(context.Background())
 	if err != nil {
-		log.Fatalf("Unable to Ping databaseL %v", err)
+		dbconn.Close()
+		return fmt.Errorf("unable to ping db: %w", err)
 	}
 
 	connection, err := dbconn.Acquire(context.Background())
 	if err != nil {
 		log.Fatal("Error Aquiring the connection from database")
 	}
+
 	defer connection.Release()
 
 	fmt.Printf("Successfully Connected to Database\n")
+	DBConn = dbconn
+	return nil
+}
 
+func initializeAccountsTable() error {
+	_, err := DBConn.Exec(context.Background(), `
+		CREATE TABLE IF NOT EXISTS useraccount (
+			uuid UUID UNIQUE PRIMARY KEY,
+			username VARCHAR(100) UNIQUE, 
+			password VARCHAR(100),
+			creation TIMESTAMP
+		)
+	`)
+	if err != nil {
+		return fmt.Errorf("error creating useraccount table: %v", err)
+	}
+
+	return nil
 }
 
 func Main() {
 	start := time.Now()
-	ConnectDatabase()
+	err := ConnectDatabase()
+	if err != nil {
+		log.Fatalf("Error Calling Connect Database function: %v", err)
+	}
+
+	err = initializeAccountsTable()
+	if err != nil {
+		log.Fatalf("Error initializing accounts table: %v", err)
+	}
 	end := time.Now()
 	duration := end.Sub(start)
 	fmt.Println(duration.String())
--- loginapi/internal/signup/signup.go
+++ loginapi/internal/signup/signup.go
@@ -1,9 +1,12 @@
 package signup
 
 import (
-	//"net/http"
-	"fmt"
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+
+	dbconn "github.com/vchen7629/cyphria/login-api/internal/db_connection"
 )
 
 type SignUpUserInfo struct {
@@ -12,10 +15,37 @@ type SignUpUserInfo struct {
 	Password string `json:"password"`
 }
 
-type SignUpHandler interface {
-	CreateNewUser()
+func CreateNewUser() error {
+	_, err := dbconn.DBConn.Exec(context.Background(), `
+		INSERT INTO useraccount (uuid, username, password, creation)
+		VALUES (
+			'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a26',
+			'tribbie',
+			'sunday',
+			'2025-03-10 19:58:39'
+		) 
+		ON CONFLICT (username)
+		DO NOTHING;
+	`)
+
+	if err != nil {
+		log.Fatalf("Error creating a new user: %v", err)
+	}
+	
+	return nil
 }
 
-func CreateNewUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello from create new user")
+func HttpHandler(w http.ResponseWriter, r *http.Request) {
+	if err := CreateNewUser(); err != nil {
+		http.Error(w, "Error Creating User", http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]string{
+		"message": "user created Successfully!",
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
 }

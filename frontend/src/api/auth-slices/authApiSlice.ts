d5475ac9a4fd7ef42a500ebcd393cad4ef334968
Vchen7629 <chenvincent7629@gmail.com>
2025-03-15 01:22:04 -0700

    fixed initial login into a new account while logged in bug

--- frontend/src/api/auth-slices/authApiSlice.ts
+++ frontend/src/api/auth-slices/authApiSlice.ts
@@ -1,4 +1,5 @@
 import { baseSlice } from "../base/baseSlice";
+import { setCredentials } from "../state/authstate";
 
 export const authApiSlice = baseSlice.injectEndpoints({
     endpoints: builder => ({
@@ -9,6 +10,16 @@ export const authApiSlice = baseSlice.injectEndpoints({
                 body: { ...credentials},   
                 credentials: 'include', 
             }),
+            async onQueryStarted(_, { dispatch, queryFulfilled }) {
+                try {
+                    const { data } = await queryFulfilled
+                    const { uuid, username } = data.user
+                    const result = dispatch(setCredentials({ uuid, username }))
+                    console.log(result)
+                } catch (err) {
+                    console.error('Login Error', err)
+                }
+            }
         }),
         Logout: builder.mutation({
             query: () => ({

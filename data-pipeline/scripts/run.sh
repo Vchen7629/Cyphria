91739194b14d57d8865645106f52f1469d65ac0c
Vchen7629 <chenvincent7629@gmail.com>
2025-04-06 04:11:15 -0700

    created dockerfile for containerizing my data-pipeline processing

---
+++ data-pipeline/scripts/run.sh
@@ -0,0 +1,11 @@
+set -e
+
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\u001b[31m'
+NC='\033[0m'
+
+cd ..
+echo -e "${YELLOW}Preparing to start DockerFile...${NC}"
+docker run --restart on-failure:5 -e KAFKA_BOOTSTRAP_SERVERS='host.docker.internal:9094' -d data-processing-pipeline
+echo -e "${GREEN}Successfully Started DockerFile!${NC}" 

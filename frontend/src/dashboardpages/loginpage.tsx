b8cc5c289e19a5d8c2fbde65ac20cd73076c5135
Vchen7629 <chenvincent7629@gmail.com>
2025-05-05 03:23:13 -0700

    testing to see if argocd image updater picks up changes

--- frontend/src/dashboardpages/loginpage.tsx
+++ frontend/src/dashboardpages/loginpage.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
             <section className="flex flex-col space-y-[2vh] items-center z-10 w-[25vw] h-[55vh] bg-[#141414] rounded-3xl py-[5vh]">
                 <LoginLogo />
                 <span className="text-3xl font-semibold">Welcome Back</span>
-                <span className="text-sm text-gray-400 font-semibold">Don't have an account yet? Sign up</span>
+                <span className="text-sm text-gray-400 font-semibold">Don't have an account yet? Sign up!</span>
                 <LoginForm/>
             </section>
 

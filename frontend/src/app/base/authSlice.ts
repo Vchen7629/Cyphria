131f451bccfd0271a89bd3e611eee9d512b781e5
Vchen7629 <chenvincent7629@gmail.com>
2025-05-09 09:33:05 -0700

    switching query for loginapi to be served with https instead of http

--- frontend/src/app/base/authSlice.ts
+++ frontend/src/app/base/authSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const authApiQuery = fetchBaseQuery({
     //baseUrl: 'http://localhost:3000',
-    baseUrl: 'http://loginapi-service-backend-svc-cluster-local:449',
+    baseUrl: 'https://loginapi-service-backend-svc-cluster-local:449',
     credentials: 'include',
 })
 

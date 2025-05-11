2dd2e6291821b549f9c9799269ebc38147c717bf
Vchen7629 <chenvincent7629@gmail.com>
2025-05-11 14:25:25 -0700

    fix: forgot to remove load Env File from login api causing issues

--- loginapi/main.go
+++ loginapi/main.go
@@ -9,21 +9,12 @@ import (
     "syscall"
 	"context"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 	"github.com/Vchen7629/Cyphria/loginapi/config/middleware"
 	"github.com/Vchen7629/Cyphria/loginapi/config/postgres"
 	"github.com/Vchen7629/Cyphria/loginapi/config/redis"
 	"github.com/Vchen7629/Cyphria/loginapi/internal/accountComponents"
 	"github.com/Vchen7629/Cyphria/loginapi/internal/authenticatedRequests"
 )
-
-
-func LoadEnvFile() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file\n")
-	}	
-}
 
 func RouteHandlers(r *mux.Router) {
 	r.HandleFunc("/", helloWorld)

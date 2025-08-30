2f9bd91de5fbc52971e187e72105cad6ac80f448
Vchen7629 <chenvincent7629@gmail.com>
2025-08-30 02:05:29 -0700

    Python Script used to create the csv file containing the example reddit posts for the specific subreddit.

--- data_processing_pipeline/category_classification_service/preprocessing/export_csv.py
+++ data_processing_pipeline/category_classification_service/preprocessing/export_csv.py
@@ -1,15 +1,11 @@
 import csv, os
 
-def exportCSV(body, subreddit):#, upvotes, downvotes, timestamp):
-    header = [
-        'body', 'subreddit', 'category',
-    ]
+def exportCSV(body: str, subreddit: str) -> None:#, upvotes, downvotes, timestamp):
+    header = ['body', 'subreddit', 'category']
 
-    data = [
-        body, subreddit, ''
-    ]
+    data = [body, subreddit, '']
 
-    file_name = 'reddit-posts3.csv'
+    file_name = 'vehicle1.csv'
     script_dir = os.path.dirname(os.path.abspath(__file__))
     target_dir = os.path.join(script_dir, '..', 'datasets')
     file_path = os.path.join(target_dir, file_name)

e2f5d36cd2469bcbb2e3c151bc43688c97ccaf87
Vchen7629 <chenvincent7629@gmail.com>
2025-10-06 15:27:02 -0700

    refactor: removed login api since not needed

--- monitoring/temp/pgconfig.go
+++ monitoring/temp/pgconfig.go
@@ -1,41 +1,11 @@
 package postgres
 
 import (
-	"os"
 	"log"
-	"fmt"
+	"os"
 	"time"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
-
-func PgConn() string {
-	host := os.Getenv("PG_HOST")
-	port := os.Getenv("PG_PORT")
-	user := os.Getenv("PG_USER")
-	password := os.Getenv("PG_PASS")
-	dbname := os.Getenv("PG_DB")
-	sslmode := "allow"
-
-	if host == "" {
-		log.Fatal("No Host provided")
-	} else if port == "" {
-		log.Fatal("No Port provided")
-	} else if user == "" {
-		log.Fatal("No user provided")
-	} else if password == "" {
-		log.Fatal("No password provide")
-	} else if dbname == "" {
-		log.Fatal("No dbname provided")
-	}
-
-	databaseuri := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", user, password, host, port, dbname, sslmode)
-
-	if databaseuri == "" {
-		return ""
-	}
-
-	return databaseuri
-}
 
 func PoolConfig() *pgxpool.Config {
 	const DefaultMaxConns = int32(4)
@@ -46,15 +16,10 @@ func PoolConfig() *pgxpool.Config {
 	const DefaultHealthCheckPeriod = time.Minute
 	const DefaultConnectTimeout = time.Second * 5
 
-	DatabaseURI := PgConn()
-
-	if DatabaseURI == "" {
-		log.Fatalln("No database uri provided!")
-	}
-
+	DatabaseURI := os.Getenv("DATABASEURI")
 	poolConfig, err := pgxpool.ParseConfig(DatabaseURI)
 	if err != nil {
-		log.Fatalf("Error Applying Config: %v", err)
+		log.Fatal("Error Applying Config")
 	}
 
 	poolConfig.MaxConns = DefaultMaxConns

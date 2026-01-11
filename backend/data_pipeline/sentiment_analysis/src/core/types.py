8cb4555226ccb12e3a399bcb5fd8f8749fed3c72
Vchen7629 <chenvincent7629@gmail.com>
2026-01-11 10:46:13 -0800

    chore(sentiment_analysis): created core folder and moved kafka config, logger, model, and types files into it

--- backend/data_pipeline/sentiment_analysis/src/core/types.py
+++ backend/data_pipeline/sentiment_analysis/src/core/types.py
@@ -1,9 +1,12 @@
 from typing import TypedDict
 
-
 class QueueMessage(TypedDict):
     topic: str
     partition: int
     offset: int
     postID: str
     postBody: str
+    subreddit: str
+    timestamp: str  # ISO format datetime string
+    score: int
+    author: str

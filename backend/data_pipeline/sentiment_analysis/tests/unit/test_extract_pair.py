ce90c70d01ce84073e0293bef551443e04ca913f
Vchen7629 <chenvincent7629@gmail.com>
2026-01-11 10:55:34 -0800

    test(sentiment analysis): updated test extract pair unit test to work with the updated extract pair method

--- backend/data_pipeline/sentiment_analysis/tests/unit/test_extract_pair.py
+++ backend/data_pipeline/sentiment_analysis/tests/unit/test_extract_pair.py
@@ -4,49 +4,48 @@ from src.preprocessing.extract_pairs import extract_pairs
 
 # Mocking Kafka Message
 class MockMessage:
-    def __init__(self, value: dict):
+    def __init__(self, value: dict[str, str | list[str]]) -> None:
         self._value = json.dumps(value).encode("utf-8")
 
     def value(self) -> bytes:
         return self._value
 
 
-# Happy path: expected behavior
 def test_normal() -> None:
+    """mock message with the body and product list"""
     msg = MockMessage(
-        value={"body": "This is test post about cats and dogs", "keywords": ["cats", "dogs"]}
+        value={"comment_body": "This is test post about cats and dogs", "detected_products": ["cats", "dogs"]}
     )
 
-    result = extract_pairs(msg)
+    result = extract_pairs(msg.value().decode("utf-8"))
 
     assert result == [
         ("This is test post about cats and dogs", "cats"),
         ("This is test post about cats and dogs", "dogs"),
     ]
 
+def test_no_comment_body() -> None:
+    """No comment body in value edge case"""
+    msg = MockMessage(value={"comment_body": "", "detected_products": ["cats", "dogs"]})
 
-# edge case: no body in value
-def test_no_body() -> None:
-    msg = MockMessage(value={"body": "", "keywords": ["cats", "dogs"]})
-
-    result = extract_pairs(msg)
-
-    assert result == []
-
-
-# edge case: no keywords in value
-def test_no_keywords() -> None:
-    msg = MockMessage(value={"body": "This is test post about cats and dogs", "keywords": []})
-
-    result = extract_pairs(msg)
+    result = extract_pairs(msg.value().decode("utf-8"))
 
     assert result == []
 
 
-# edge case: no keywords and body in value
-def test_no_keywords_body() -> None:
-    msg = MockMessage(value={"body": "", "keywords": []})
+def test_no_detected_products() -> None:
+    """No detected products list in value edge case"""
+    msg = MockMessage(value={"comment_body": "This is test post about cats and dogs", "detected_products": []})
 
-    result = extract_pairs(msg)
+    result = extract_pairs(msg.value().decode("utf-8"))
+
+    assert result == []
+
+
+def test_no_comment_body_or_detected_products() -> None:
+    """No comment body or detected_product list in mock message edge case"""
+    msg = MockMessage(value={"comment_body": "", "detected_products": []})
+
+    result = extract_pairs(msg.value().decode("utf-8"))
 
     assert result == []

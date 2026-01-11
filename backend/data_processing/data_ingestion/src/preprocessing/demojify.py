0d89ba80bbaeed84644d90c01c18d22ca1d76686
Vchen7629 <chenvincent7629@gmail.com>
2026-01-11 00:04:12 -0800

    chore(data_ingestion): added docstrings to demojify.py and url_remover.py for better documentation

--- backend/data_processing/data_ingestion/src/preprocessing/demojify.py
+++ backend/data_processing/data_ingestion/src/preprocessing/demojify.py
@@ -1,8 +1,20 @@
 import emoji
 import re
 
-def demojify(text: str) -> str:
-    demojized = emoji.demojize(text)
-    add_bracket = re.sub(r":([a-zA-Z0-9_]+):", r"[\1]", demojized)
+def demojify(comment_text: str) -> str:
+    """
+    Helper function for converting emoji characters into plain text
 
-    return add_bracket
+    Args:
+        comment_text: raw comment text that may or may not have emojis
+
+    Returns:
+        demojized_text_with_bracket: comment text with emojis converted to
+                                    a string with brackets around it
+    """
+    demojized: str = emoji.demojize(comment_text)
+
+    # add bracket delimiter around demojized text to seperate it
+    demojized_text_with_bracket: str = re.sub(r":([a-zA-Z0-9_]+):", r"[\1]", demojized)
+
+    return demojized_text_with_bracket
--- backend/data_processing/data_ingestion/src/preprocessing/url_remover.py
+++ backend/data_processing/data_ingestion/src/preprocessing/url_remover.py
@@ -1,14 +1,17 @@
 import re
 
+def remove_url(comment_text: str) -> str:
+    """
+    Helper function to remove url from comment body text using regex
 
-def remove_url(
-    rawdata: str,
-) -> str:
+    Args:
+        comment_text: comment text string that may or may not contain a url
+
+    Returns:
+        filtered: comment text string with url replaced by empty space
+    """
     url_pattern = re.compile(r"(?:https?://\S+|www\.\S+|\b\w+\.\w+\b)")
 
-    result = url_pattern.sub(
-        "",
-        rawdata,
-    )
+    filtered: str = url_pattern.sub("", comment_text)
 
-    return result
+    return filtered

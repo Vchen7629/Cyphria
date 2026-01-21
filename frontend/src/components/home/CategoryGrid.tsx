45ff8d2f7d4e38044fef8c9c04627353048e9c09
Vchen7629 <chenvincent7629@gmail.com>
2026-01-20 16:51:39 -0800

    style(frontend): added slice restriction to category grid on homepage so it only renders 6 categories for browse category

--- frontend/src/components/home/CategoryGrid.tsx
+++ frontend/src/components/home/CategoryGrid.tsx
@@ -11,7 +11,7 @@ const CategoryGrid = ({ categories }: CategoryComponentProps) => {
       <h2 className="text-lg font-medium text-zinc-100">Browse Categories</h2>
       <span className="text-sm font-light text-zinc-400"></span>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-        {categories.map((category) => (
+        {categories.slice(0,6).map((category) => (
           <CategoryCard key={category.id} category={category} />
         ))}
       </div>

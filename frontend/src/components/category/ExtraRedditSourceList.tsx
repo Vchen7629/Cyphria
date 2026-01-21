5762dcc252d345d7d3f665ee7c8208804bb51609
Vchen7629 <chenvincent7629@gmail.com>
2026-01-21 07:30:51 -0800

    refactor(frontend): moved RedditSourcePill to be inline in ExtraRedditSourceList to reduce file bloat

--- frontend/src/components/category/ExtraRedditSourceList.tsx
+++ frontend/src/components/category/ExtraRedditSourceList.tsx
@@ -1,7 +1,6 @@
 import { ChevronDown } from "lucide-react";
 import { Subreddit } from "../../mock/types";
 import { useState } from "react";
-import RedditSourcePill from "./RedditSourcePill";
 
 interface ExtraRedditSourceProps {
     subreddits: Subreddit[]
@@ -25,7 +24,14 @@ const ExtraRedditSourceList = ({ subreddits, totalExtra }: ExtraRedditSourceProp
                 <ul className="flex flex-col space-y-2 absolute w-[140%] max-h-30 overflow-auto mt-2 left-[-20%] py-2 px-2 bg-zinc-800 rounded-lg text-zinc-400 z-50">
                     <span className="text-xs ml-2">+{totalExtra} extra sources</span>
                     {subreddits.map((subreddit: Subreddit) => (
-                        <RedditSourcePill subreddit={subreddit}/>
+                        <a
+                            href={`https://www.reddit.com/r/${subreddit.name}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="flex items-center py-1 px-3 bg-orange-600 rounded-lg text-white border border-orange-700 hover:text-zinc-200 hover:border-orange-400 transition-colors duration-250"
+                        >
+                            <span className="text-xs truncate">r/{subreddit.name}</span>
+                        </a>
                     ))}
                 </ul>
             )}

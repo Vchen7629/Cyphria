0f3831f217dbccd5400f487ade92332388f3df9f
Vchen7629 <chenvincent7629@gmail.com>
2026-01-26 08:06:59 -0800

    fix(insights api): fixed histogram bucket not being sorted

--- backend/insights_api/src/middleware/metrics.py
+++ backend/insights_api/src/middleware/metrics.py
@@ -6,7 +6,7 @@ db_query_duration = Histogram(
     'Database query duration in seconds',
     ['query_type', 'query_name', 'table'],
     # time frame in seconds
-    buckets=(0.05, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5)
+    buckets=(0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5),
 )
 
 cache_operation_duration = Histogram(

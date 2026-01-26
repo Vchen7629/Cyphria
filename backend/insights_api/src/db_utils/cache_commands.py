1c922b16c0c4b7c161e69645393b4a12fa796a1d
Vchen7629 <chenvincent7629@gmail.com>
2026-01-25 16:36:54 -0800

    feat(insight api): created reusable methods for the get and set valkey commands

---
+++ backend/insights_api/src/db_utils/cache_commands.py
@@ -0,0 +1,104 @@
+from typing import Any
+from typing import TypeVar
+from typing import Optional
+from typing import Callable
+from datetime import datetime
+from pydantic import BaseModel
+from valkey.asyncio import Valkey
+from src.core.logger import StructuredLogger
+
+T = TypeVar('T', bound=BaseModel)
+
+async def get_cache_value(
+    cache: Valkey, 
+    cache_key: str, 
+    logger: StructuredLogger, 
+    response_model: type[T],
+) -> Optional[T]:
+    """
+    Fetch the value of a cache_key from the caching layer (Valkey)
+
+    Args:
+        cache: the cache_client connection
+        cache_key: the key in the cache with the values we are trying to fetch
+        logger: StructuredLogger for logging
+        response_model: pydantic type class we are returning as a api response
+    
+    Returns:
+        the pydantic type class with api response or None if not found/error
+    """
+    try:
+        cached = await cache.get(cache_key)
+        if cached:
+            return response_model.model_validate(cached)
+            
+    except Exception as e:
+       logger.warning(event_type="insights_api run", message=f"Cache read failed, falling back to db: {e}")
+
+    return None
+
+async def set_cache_value(
+    cache: Valkey,
+    cache_key: str,
+    value: BaseModel,
+    ttl_seconds: int,
+    logger: StructuredLogger,
+    on_cache_write: Optional[Callable[[Valkey], Any]] = None
+) -> None:
+    """
+    Set value in cache (Valkey) with optional side effects
+
+    Args:
+        cache: Cache (Valkey) client connection
+        cache_key: cache_key to set in the db
+        value: Pydantic Model to cache
+        ttl_seconds: Expiry time in seconds
+        logger: StructuredLogger instance
+        on_cache_write: Optional async callback for additional cache ops
+    """
+    try:
+        await cache.setex(cache_key, ttl_seconds, value.model_dump_json())
+
+        # Execute optional side effect like updating trending sorted set
+        if on_cache_write:
+            await on_cache_write(cache)
+
+    except Exception as e:
+        logger.warning(event_type="insights_api run", message=f"Cache write failed for key {cache_key}: {e}")
+    
+def get_weekly_trending_key() -> str:
+    """
+    Generate weekly trending key that auto-resets each week
+
+    Returns:
+        key like "trending:topics:2026-W04"
+    """
+    week_info = datetime.now().isocalendar()
+    
+    return f"trending:topics:{week_info[0]}-W{week_info[1]}"
+
+async def increment_trending_topic(
+    cache: Valkey, 
+    topic: str, 
+    client_ip: str,
+    weekly_ttl: int = 604800 # 7 days represented in seconds
+) -> None:
+    """
+    Increment topic view count in weekly trending sorted set
+    only increments if ip is different, prevents refresh spam
+    
+    Args:
+        cache: cache (Valkey) client connection
+        topic: topic name to increment
+        client_ip: client ip used to make sure it only increments with unique ips
+        weekly_ttl: TTL for trending key (default is 7 days)
+    """
+    week_info = datetime.now().isocalendar()
+    view_key: str = f"viewed:{topic}:{client_ip}:{week_info[1]}"
+
+    is_new_view = await cache.set(view_key, "1", ex=weekly_ttl, nx=True)
+    if is_new_view:
+        trending_key = get_weekly_trending_key()
+        await cache.zincrby(trending_key, 1, topic)
+        await cache.expire(trending_key, weekly_ttl)
+

e5ea56e356e2cebd1369deec93ba6a9ef194745c
Vchen7629 <chenvincent7629@gmail.com>
2026-02-05 14:25:26 -0800

    refactor(orchestrator): updated dags to work with the list of topics and subreddits

--- backend/data_pipeline/orchestrator/src/dags/product_topic_ranking.py
+++ backend/data_pipeline/orchestrator/src/dags/product_topic_ranking.py
@@ -1,56 +1,62 @@
-import json
 from airflow.sdk import DAG
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import HttpOperator
 from src.config.settings import Settings
-from src.config.topic_mappings import ProductTopicMappings
 from src.utils.on_task_failure import on_task_failure
+import json
 
 settings = Settings()
 
-
-def create_ranking_dag(product_topic: str) -> DAG:
+def create_ranking_dag(category: str, topic_list: list[str]) -> DAG:
     """
-    Create a ranking DAG for a specific product_topic.
-    
+    Create a ranking DAG for a specific category
+
     Args:
-        product_topic: the product topic we are ranking
+        category: the category we are ranking
+        topic_list: list of topics for the current category
 
     Returns:
-        a dag that have tasks that fetch and rank products for the specific topic
+        a dag that have tasks that fetch and rank products for the specific category
     """
+    from src.config.schedules import RANKINGSCHEDULES
+
     dag = DAG(
-        dag_id=f"product_{product_topic.lower()}_ranking",
-        schedule=ProductTopicMappings.RANKING_SCHEDULES.get(product_topic),
+        dag_id=f"product_{category.lower().replace(' ', '_')}_ranking",
+        schedule=RANKINGSCHEDULES.get(category.upper()),
         start_date=settings.START_DATE,
         catchup=False,
-        tags=['ranking', product_topic.lower()],
+        tags=['ranking', category.lower().replace(' ', '_')],
         max_active_runs=settings.MAX_ACTIVE_RUNS,
-        doc_md="""
-        ## Ranking DAG
-        Reads from product_sentiment table for a product_topic and creates ranking
+        params={
+            'category': category,
+            'topics': topic_list
+        },
+        doc_md=f"""
+        ## Ranking DAG for {category}
+        Reads from product_sentiment table for a category and creates ranking
         related metadata like:
 
         1. Assigning a letter grade (S, A+, A, etc)
         2. Calculate the Bayesian rated score for each product
-        3. Assign the ranks (1-indexed) for all products in the product_topic based on the score
+        3. Assign the ranks (1-indexed) for all products in the category based on the score
         4. Add tags like is_top_rated, is_most_discussed, and has_limited_data
 
         for both a 90day and all_time time window and writes to the product_ranking table
 
+        **Topics:** {', '.join(topic_list)}
         **Schedule:** Daily at xx:30
         """
     )
 
     with dag:
         product_ranking_all_time = HttpOperator(
-            task_id=f'rank_{product_topic.lower()}_products_all_time',
+            task_id=f'rank_{category.lower().replace(' ', '_')}_products_all_time',
             http_conn_id="product_ranking_service",
             endpoint='/run',
             method='POST',
             headers={'Content-Type': 'application/json'},
             data=json.dumps({
-                'product_topic': product_topic,
+                'topic_list': topic_list,
                 'time_windows': "all_time",
                 'bayesian_params': "30"
             }),
@@ -66,7 +72,7 @@ def create_ranking_dag(product_topic: str) -> DAG:
         
         # Wait for all_time task to finish by polling /status endpoint until its done
         wait_all_time = HttpSensor(
-            task_id=f"wait_rank_{product_topic.lower()}_products_all_time",
+            task_id=f"wait_rank_{category.lower().replace(' ', '_')}_products_all_time",
             http_conn_id="product_ranking_service",
             endpoint="/status",
             response_check=lambda response: response.json()['status'] in ["completed", "cancelled"],
@@ -79,13 +85,13 @@ def create_ranking_dag(product_topic: str) -> DAG:
         ) 
 
         product_ranking_90_days = HttpOperator(
-            task_id=f'rank_{product_topic.lower()}_products_90_day',
+            task_id=f'rank_{category.lower().replace(' ', '_')}_products_90_day',
             http_conn_id="product_ranking_service",
             endpoint='/run',
             method='POST',
             headers={'Content-Type': 'application/json'},
             data=json.dumps({
-                'product_topic': product_topic,
+                'topic_list': topic_list,
                 'time_windows': "90d",
                 'bayesian_params': "30"
             }),
@@ -102,7 +108,7 @@ def create_ranking_dag(product_topic: str) -> DAG:
         # Wait for 90_day task to finish by polling /status endpoint until its done
         # Lets airflow know if it failed or not
         wait_90_day = HttpSensor(
-            task_id=f"wait_rank_{product_topic.lower()}_products_90_day",
+            task_id=f"wait_rank_{category.lower().replace(' ', '_')}_products_90_day",
             http_conn_id="product_ranking_service",
             endpoint="/status",
             response_check=lambda response: response.json()['status'] in ["completed", "cancelled"],
@@ -118,7 +124,11 @@ def create_ranking_dag(product_topic: str) -> DAG:
 
     return dag
 
+import sys
+if 'pytest' not in sys.modules:
+    from src.config.mappings import CATEGORYTOPIC
 
-# Create DAGs at module level so Airflow can discover them
-for _topic in ProductTopicMappings.TOPICS:
-    globals()[f'product_{_topic.lower()}_ranking'] = create_ranking_dag(_topic)
+    for _category in CATEGORYTOPIC:
+        topic_list = CATEGORYTOPIC.get(_category, [])
+
+        globals()[f'product_{_category.lower().replace(" ", "_")}_ranking'] = create_ranking_dag(_category, topic_list)

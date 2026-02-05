bae25f687280dc46e6da1344c2c44c157267fa5b
Vchen7629 <chenvincent7629@gmail.com>
2026-02-05 14:24:51 -0800

    feat(orchestrator): created utility function for extracting the unique list of subreddits for a category

---
+++ backend/data_pipeline/orchestrator/src/utils/extract_subreddit_list.py
@@ -0,0 +1,22 @@
+from src.config.mappings import TOPICSUBREDDIT
+from src.config.mappings import CATEGORYTOPIC
+
+def extract_subreddit_list(category: str) -> list[str]:
+    """
+    Build a set containing a list of all unique subreddits that need
+    to be fetched from for the current product category
+
+    Args:
+        category: the category we are processing
+
+    Returns:
+        a list containing unique subreddit strings
+    """
+    topics = CATEGORYTOPIC.get(category, [])
+
+    subreddits = set()
+    for topic in topics:
+        topic_subreddits = TOPICSUBREDDIT.get(topic, [])
+        subreddits.update(topic_subreddits)
+
+    return list(subreddits)
